6e53e286c35ec889250b3812225c8972f85a00a9
Matthew Zenn <spyguy57@gmail.com>
2024-03-02 15:15:28 -0500

    built site

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById('github').addEventListener('click', () => {
+  document.documentElement.setAttribute('theme', 'github');
+});
+
+document.getElementById('twitter').addEventListener('click', () => {
+  document.documentElement.setAttribute('theme', 'twitter');
+});
+
+document.getElementById('google').addEventListener('click', () => {
+  document.documentElement.setAttribute('theme', 'google');
+});
+
+document.getElementById('reddit').addEventListener('click', () => {
+  document.documentElement.setAttribute('theme', 'reddit');
+});
